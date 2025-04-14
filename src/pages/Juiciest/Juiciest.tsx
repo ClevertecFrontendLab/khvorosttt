52c67ab6ff61dfcc72dc1f401ee3688fee76a816
khvorosttt <khvorosttt@yandex.by>
2025-04-14 11:08:31 +0300

    feat(pages): implement Juiciest page

---
+++ src/pages/Juiciest/Juiciest.tsx
@@ -0,0 +1,29 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+import { Search } from '~/components/Search/Search';
+import authors from '~/data/authorsPosts.json';
+import juiciest from '~/data/juiciest.json';
+import newData from '~/data/new.json';
+import shortInfo from '~/data/vegan.json';
+import { CategoryInfo } from '~/sections/CategoryInfoSection/CategoryInfo';
+import { CookingBlog } from '~/sections/CookingBlog/CookingBlog';
+import { Juiciest } from '~/sections/Juiciest/Juiciest';
+import { NewRecipes } from '~/sections/NewRecipes/NewRecipes';
+
+export function JuiciestPage() {
+    return (
+        <Box w='100%' p='90px 0px'>
+            <Flex flexDirection='column' gap='40px'>
+                <Search name='Самое сочное' description={null} />
+                <NewRecipes data={newData} />
+                <Juiciest data={juiciest} />
+                <CookingBlog data={authors} />
+                <CategoryInfo
+                    name={shortInfo.name}
+                    description={shortInfo.description}
+                    data={shortInfo.data}
+                />
+            </Flex>
+        </Box>
+    );
+}
