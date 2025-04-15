e4196b73432c1004372b9be99969950ef05364b5
khvorosttt <khvorosttt@yandex.by>
2025-04-15 13:07:45 +0300

    refactor(PostCard): move Postcard component styles to a separate file

---
+++ src/components/PostCard/PostCard.style.ts
@@ -0,0 +1,23 @@
+import type { SystemStyleObject } from '@chakra-ui/styled-system';
+
+export const CardHeadingStyle: SystemStyleObject = {
+    overflow: 'hidden',
+    fontWeight: 500,
+    fontSize: { base: '16', lg: '18px' },
+    fontFamily: 'text',
+};
+
+export const CardEmailStyle: SystemStyleObject = {
+    color: 'rgba(0, 0, 0, 0.64)',
+    fontWeight: 400,
+    fontSize: { base: '12px', lg: '14px' },
+    fontFamily: 'text',
+};
+
+export const CardDescriptionStyle: SystemStyleObject = {
+    noOfLines: 3,
+    fontSize: '14px',
+    fontFamily: 'text',
+    overflow: 'hidden',
+    whiteSpace: 'normal',
+};
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,5 +1,7 @@
 import { Avatar, Card, CardBody, CardHeader, Heading, HStack, Stack, Text } from '@chakra-ui/react';
 
+import { CardDescriptionStyle, CardEmailStyle, CardHeadingStyle } from './PostCard.style';
+
 export type AuthorTypeInfo = {
     name: string;
     email: string;
@@ -18,36 +20,15 @@ export function PostCard({ data }: AuthorType) {
                 <HStack w='100%'>
                     <Avatar name={data.name} src={data.src} />
                     <Stack gap='2px' w='100%' overflow='hidden'>
-                        <Heading
-                            size='sm'
-                            overflow='hidden'
-                            fontWeight={500}
-                            fontSize={{ base: '16', lg: '18px' }}
-                            fontStyle='text'
-                            isTruncated
-                        >
+                        <Heading size='sm' sx={CardHeadingStyle} isTruncated>
                             {data.name}
                         </Heading>
-                        <Text
-                            color='rgba(0, 0, 0, 0.64)'
-                            fontWeight={400}
-                            fontSize={{ base: '12px', lg: '14px' }}
-                            fontStyle='text'
-                        >
-                            {data.email}
-                        </Text>
+                        <Text sx={CardEmailStyle}>{data.email}</Text>
                     </Stack>
                 </HStack>
             </CardHeader>
             <CardBody padding='8px 16px 16px 16px' w='100%'>
-                <Text
-                    noOfLines={3}
-                    fontSize='14px'
-                    fontFamily='text'
-                    overflow='hidden'
-                    isTruncated
-                    whiteSpace='normal'
-                >
+                <Text sx={CardDescriptionStyle} isTruncated>
                     {data.text}
                 </Text>
             </CardBody>
