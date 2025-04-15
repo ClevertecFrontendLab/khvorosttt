4e8c683790ca9575536557c136935768c3a867de
khvorosttt <khvorosttt@yandex.by>
2025-04-15 10:47:43 +0300

    refactor(pages): use DisplayFood element for display food cards

--- src/pages/Juiciest/Juiciest.tsx
+++ src/pages/Juiciest/Juiciest.tsx
@@ -1,17 +1,17 @@
 import { Box, Flex } from '@chakra-ui/react';
 
+import { FoodDisplay } from '~/components/FoodDisplay/FoodDisplay';
 import { Search } from '~/components/Search/Search';
 import juiciest from '~/data/juiciest.json';
 import shortInfo from '~/data/vegan.json';
 import { CategoryInfo } from '~/sections/CategoryInfoSection/CategoryInfo';
-import { Juiciest } from '~/sections/Juiciest/Juiciest';
 
 export function JuiciestPage() {
     return (
         <Box w='100%' p='90px 0px'>
             <Flex flexDirection='column' gap='40px'>
                 <Search name='Самое сочное' description={null} />
-                <Juiciest data={juiciest} />
+                <FoodDisplay data={juiciest} />
                 <CategoryInfo
                     name={shortInfo.name}
                     description={shortInfo.description}
