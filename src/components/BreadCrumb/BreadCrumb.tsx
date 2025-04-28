603794131dadcbeb47f8df0d14a50017129acb17
khvorosttt <khvorosttt@yandex.by>
2025-04-28 15:53:52 +0300

    feat(pages): add breadcrumds routing

--- src/components/BreadCrumb/BreadCrumb.tsx
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -1,23 +1,31 @@
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
-import { Link, useLocation } from 'react-router';
+import { Link, useLocation, useParams } from 'react-router';
 
+import { markerFood } from '~/data/consts';
+import menuRecipes from '~/data/menuData.json';
+
+import { BurgerMenuProps } from '../BurgerMenu/BurgerMenu';
 import { BreadCrumbStyle } from './BreadCrumb.style';
 
-const linkName: Record<string, string> = {
-    'vegan-cuisine': 'Веганская кухня',
-    'the-juiciest': 'Самое сочное',
-};
-
-export function BreadCrumb() {
+export function BreadCrumb({ isOpen, toggleMenu }: BurgerMenuProps) {
     const location = useLocation();
     const paths = location.pathname.split('/').filter((x) => x);
+    const { id } = useParams();
     return (
         <Breadcrumb
             spacing='8px'
             separator={<ChevronRightIcon color='gray.500' />}
             p='0px 20px'
             data-test-id='breadcrumbs'
+            listProps={{
+                style: {
+                    margin: 0,
+                    padding: 0,
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                },
+            }}
         >
             <BreadcrumbItem>
                 <BreadcrumbLink
@@ -25,6 +33,9 @@ export function BreadCrumb() {
                     to='/'
                     sx={BreadCrumbStyle}
                     color={paths.length == 0 ? 'black' : 'rgba(0, 0, 0, 0.64)'}
+                    onClick={() => {
+                        isOpen && toggleMenu();
+                    }}
                 >
                     Главная
                 </BreadcrumbLink>
@@ -36,8 +47,13 @@ export function BreadCrumb() {
                         to={`/${paths.slice(0, index + 1).join('/')}`}
                         sx={BreadCrumbStyle}
                         color={index + 1 == paths.length ? 'black' : 'rgba(0, 0, 0, 0.64)'}
+                        onClick={() => {
+                            isOpen && toggleMenu();
+                        }}
                     >
-                        {linkName[linkPath] || linkPath}
+                        {id && index === paths.length - 1
+                            ? `${menuRecipes.find((item) => item.id === Number(id))?.title}`
+                            : markerFood[linkPath] || linkPath}
                     </BreadcrumbLink>
                 </BreadcrumbItem>
             ))}
